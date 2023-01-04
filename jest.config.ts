c65f65cfa92add14eec555ab8c8a1137d6662a52
Luiz Carlos <argentinaluiz@gmail.com>
2023-01-04 10:54:46 -0300

    integração com jest

--- jest.config.ts
+++ jest.config.ts
@@ -130,7 +130,7 @@ export default {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: 'src',
+  rootDir: 'dist',
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
@@ -173,7 +173,7 @@ export default {
   // ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
-  testRegex: '.*\\..*spec\\.ts$',
+  testRegex: '.*\\..*spec\\.js$',
 
   // This option allows the use of a custom results processor
   // testResultsProcessor: undefined,
@@ -183,7 +183,8 @@ export default {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '^.+\\.ts?$': ['@swc/jest'],
+    //'^.+\\.ts?$': ['@swc/jest'],
+    '^.+\\.js?$': ['@swc/jest'],
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
